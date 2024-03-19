848e09aedad0f7ac94296e53df64c6329807a2e9
Sergey Abrosov <sgabrosov@icloud.com>
2024-03-19 22:29:01 +0300

    Added project & ignoring generated files

---
+++ iOS/SiriusMapIOS/SiriusMapIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiriusMapIOS
+//
+//  Created by Sergey Abrosov on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
