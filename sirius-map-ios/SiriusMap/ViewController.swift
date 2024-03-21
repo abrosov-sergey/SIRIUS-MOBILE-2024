533c8acc42bb3d3f004a1893d351b404578d825e
Egor <egor@Egors-MacBook-Air.local>
2024-03-21 18:28:41 +0300

    Переименование iOS проекта

---
+++ sirius-map-ios/SiriusMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiriusMobileIOS
+//
+//  Created by Sergey Abrosov on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
