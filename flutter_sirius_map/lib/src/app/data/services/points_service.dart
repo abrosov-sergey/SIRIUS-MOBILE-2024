256950cab4a18ed442f3faf52ab3a7e70c18fcb6
PiggyJesus <kulakov.stanislav82@gmail.com>
2024-03-26 13:51:17 +0300

    версия ДВА

--- flutter_sirius_map/lib/src/app/data/services/points_service.dart
+++ flutter_sirius_map/lib/src/app/data/services/points_service.dart
@@ -1,73 +1,32 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
-import 'package:dijkstra/dijkstra.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sirius_map/src/app/data/services/dist.dart';
+import 'package:flutter_sirius_map/src/app/data/services/loaders/points_loader.dart';
 import 'package:flutter_sirius_map/src/app/domain/place_point.dart';
 import 'package:latlong2/latlong.dart';
 
-final pointsServiceProvider = Provider<PointsService>((_) => PointsService());
-
 class PointsService {
-  // при создании подгружаем сразу все
-  PointsService() {
-    _loadGraph();
-  }
+  final PointsLoader _pointsLoader;
+  PointsService({required PointsLoader pointsLoader})
+      : _pointsLoader = pointsLoader;
 
-  /// точки, храним оптимизированно для обращения по индексу
-  Map<int, LatLng>? _pointsById;
-
-  /// Получение точек с подгрузкой, если не обходимо
-  Future<Map<int, LatLng>> get _getPointsById async {
-    if (_pointsById == null) {
-      await _loadPoints();
-    }
-    return _pointsById!;
-  }
-
-  /// подгрузка и декодинг координат точек
-  static const _pointsPath =
-      r'flutter_sirius_map\assets\points\positions.geojson';
-  Future<void> _loadPoints() async {
-    final json = await File(_pointsPath).readAsString();
-    final info = jsonDecode(json)['features'];
-
-    _pointsById = <int, LatLng>{};
-
-    for (final line in info) {
-      int id = line['properties']['id'];
-      double lat = line['geometry']['coordinates'][0][1];
-      double lng = line['geometry']['coordinates'][0][0];
-      final ll = LatLng(lat, lng);
-
-      _pointsById![id] = ll;
-    }
-  }
+  bool containsId(int id) => _pointsLoader.pointsById.containsKey(id);
 
   /// получение точки по ее id, если не нашлась - null
-  Future<PlacePoint?> getById(int id) async {
-    final points = await _getPointsById;
-    if (points.containsKey(id)) {
+  PlacePoint? getById(int id) {
+    final points = _pointsLoader.pointsById;
+    if (containsId(id)) {
       return PlacePoint(id: id, pos: points[id]!);
     }
     return null;
   }
 
-  /// расстояние между точками
-  double _dist(LatLng l1, LatLng l2) {
-    return sqrt(pow(l1.latitude - l2.latitude, 2) +
-        pow(l1.longitude - l2.longitude, 2));
-  }
-
   /// возвращает ближайшую к данным координатам точку
   /// если отсутсвуют какие-либо точки, выкинет ошибку
-  Future<PlacePoint> getClosest(LatLng ll) async {
-    final points = await _getPointsById;
+  PlacePoint getClosest(LatLng ll) {
+    final points = _pointsLoader.pointsById;
 
     PlacePoint? ans;
     for (final entry in points.entries) {
-      if (ans == null || _dist(ll, ans.pos) > _dist(ll, entry.value)) {
+      if (ans == null || dist(ll, ans.pos) > dist(ll, entry.value)) {
         ans = PlacePoint(id: entry.key, pos: entry.value);
       }
     }
@@ -76,62 +35,5 @@ class PointsService {
       throw Exception('ans is null- probably no points found');
     }
     return ans;
-  }
-
-  Map<int, Map<int, double>>? _graph;
-
-  /// Получение графа с подгрузкой, если не обходимо
-  Future<Map<int, Map<int, double>>> get _getGraph async {
-    if (_graph == null) {
-      await _loadGraph();
-    }
-    return _graph!;
-  }
-
-  /// подгрузка графа + подсчет длины ребер
-  final String _graphPath = r'flutter_sirius_map\assets\points\Graph.txt';
-  Future<void> _loadGraph() async {
-    // для графа сразу нужны расстояния между точками
-    final points = await _getPointsById;
-
-    List<String> lines = await File(_graphPath).readAsLines();
-
-    _graph = <int, Map<int, double>>{};
-    for (var line in lines) {
-      final lineParsed = line.split(' :').map((e) => int.parse(e)).toList();
-      final v = lineParsed[0];
-      for (int i = 1; i < lineParsed.length; i++) {
-        final u = lineParsed[i];
-        if (points[v] == null || points[u] == null) {
-          continue;
-        }
-
-        final dist = _dist(points[v]!, points[u]!);
-
-        _graph![v] ??= <int, double>{};
-        _graph![v]![u] = dist;
-
-        _graph![u] ??= <int, double>{};
-        _graph![u]![v] = dist;
-      }
-    }
-  }
-
-  /// получение маршрута с помощью дейкстры
-  Future<List<PlacePoint>> getRoute(PlacePoint start, PlacePoint finish) async {
-    final graph = await _getGraph;
-    final points = await _getPointsById;
-
-    List<int> route;
-    try {
-      route = Dijkstra.findPathFromGraph(graph, start, finish) as List<int>;
-    } catch (e) {
-      if (kDebugMode) {
-        print('error while dijkstra: $e');
-      }
-      return [];
-    }
-
-    return route.map((id) => PlacePoint(id: id, pos: points[id]!)).toList();
   }
 }
