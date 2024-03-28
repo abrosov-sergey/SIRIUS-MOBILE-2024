3fb125dc33f767d113250c3c42f06b6a6e3997a0
LilyKras <62222363+LilyKras@users.noreply.github.com>
2024-03-28 16:41:42 +0300

    [SIRIUSMAP-44] qr code scanner - исправлен дизайн страницы

--- flutter_sirius_map/lib/src/features/qr_scanner/presentation/pages/qr_scanner_page.dart
+++ flutter_sirius_map/lib/src/features/qr_scanner/presentation/pages/qr_scanner_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_sirius_map/src/app/domain/providers/app_state_provider.dart';
 import 'package:flutter_sirius_map/src/core/utils/context.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -31,16 +32,40 @@ class _QrScannerPageState extends State<QrScannerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(context.localization.scan_qr),
         leading: BackButton(
           color: context.themeExtendColors.iconColor,
         ),
         backgroundColor: context.themeExtendColors.primaryColor,
       ),
       backgroundColor: context.themeExtendColors.primaryColor,
-      body: Consumer(
-        builder: (context, ref, child) => MobileScanner(
-          controller: qrController,
-          onDetect: (capture) => _onDetect(capture, context, ref),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100.h,
+            ),
+            Text(
+              textAlign: TextAlign.center,
+              context.localization.scan_qr_instruction,
+              style: context.themeExtendTextStyles.bodyStyle,
+            ),
+            SizedBox(
+              height: 40.h,
+            ),
+            Consumer(
+              builder: (context, ref, child) => Center(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: MobileScanner(
+                    controller: qrController,
+                    onDetect: (capture) => _onDetect(capture, context, ref),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
