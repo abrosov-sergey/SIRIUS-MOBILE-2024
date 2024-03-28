2dbde39c8293282f47c2359366c8531b1e59d9cb
pisarew <gapisarev@edu.hse.ru>
2024-03-28 15:06:06 +0300

    add routeView delegates

--- sirius-map-ios/SiriusMap/Route/RouteViewController.swift
+++ sirius-map-ios/SiriusMap/Route/RouteViewController.swift
@@ -9,16 +9,31 @@ import Foundation
 import SwiftUI
 import UIKit
 
+protocol RouteViewControllerDelegate: AnyObject {
+    func onTapStartCell()
+    func onTapEndCell()
+    func onChangeRoute()
+}
+
 final class RouteViewController: UIViewController {
+
+    weak var delegate: RouteViewControllerDelegate?
+    
+    private lazy var actions: RouteView.Actions = {
+        return RouteView.Actions(
+            onTapStartCell: delegate?.onTapStartCell ?? {},
+            onTapEndCell: delegate?.onTapEndCell ?? {},
+            onChangeRoute: delegate?.onChangeRoute ?? {}
+        )
+    }()
+    
     private let sheetHeight: CGFloat = 200.0
-
-    let from: String
-    let to: String
-
-    init(from: String, to: String) {
-        self.from = from
-        self.to = to
-
+    
+    private let endRouteItem: MapItem
+    
+    init(endRouteItem: MapItem) {
+        self.endRouteItem = endRouteItem
+        
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -26,9 +41,14 @@ final class RouteViewController: UIViewController {
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    private lazy var hostingController = UIHostingController(rootView: RouteView(from: from, to: to))
-
+    
+    private lazy var hostingController = UIHostingController(
+        rootView: RouteView(
+            endRouteItem,
+            actions: actions
+        )
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
