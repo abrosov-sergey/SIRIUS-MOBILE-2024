788b9bda200cf46223d20594f88783dbb9a848a8
PiggyJesus <kulakov.stanislav82@gmail.com>
2024-03-28 20:50:26 +0300

    new graph & points

--- flutter_sirius_map/lib/src/app/domain/place_point.dart
+++ flutter_sirius_map/lib/src/app/domain/place_point.dart
@@ -2,8 +2,10 @@ import 'package:latlong2/latlong.dart';
 
 class PlacePoint {
   final LatLng pos;
+  final String? name;
   final int id;
   const PlacePoint({
+    this.name,
     required this.id,
     required this.pos,
   });
